c4ebd8dfefa2ec2439db122147331fbbe95c8e8c
jjkip <jjlangatjose@gmail.com>
2023-05-29 23:52:35 +0300

    Cleaning up the user interface for betterrest app

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -10,24 +10,48 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var sleepAmount = 8.0
-    @State private var wakeUp = Date.now
+    @State private var wakeUp = defaultWakeTime
     @State private var coffeeAmount = 1
     @State private var alertTitle = " "
     @State private var alertMessage = " "
     @State private var showingAlert = false
+    
+    static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
+        return Calendar.current.date(from: components) ?? Date.now
+    }
     var body: some View {
         NavigationView {
-            VStack(spacing: 20) {
-                Text("When do you want to wake up?")
-                    .font(.headline)
-                DatePicker("Please enter a date", selection: $wakeUp, displayedComponents: .hourAndMinute)
-                    .labelsHidden()
-                Text("Desired amount of sleep")
-                    .font(.headline)
-                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
-                Text("Daily coffee intake")
-                    .font(.headline)
-                Stepper(coffeeAmount == 1 ? "1 Cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
+            Form {
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("When do you want to wake up?")
+                        .font(.headline)
+                }
+                VStack(alignment: .leading, spacing: 0) {
+                    DatePicker("Please enter a date", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                }
+               
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("Desired amount of sleep")
+                        .font(.headline)
+                }
+                
+                VStack(alignment: .leading, spacing: 0) {
+                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                }
+                
+                VStack(alignment: .leading, spacing: 0) {
+                    Text("Daily coffee intake")
+                        .font(.headline)
+                }
+                
+                VStack(alignment: .leading, spacing: 0) {
+                    Stepper(coffeeAmount == 1 ? "1 Cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
+                }
+                
     //            Text(Date.now, format: .dateTime.day().month().year())
     //            Text(Date.now.formatted(date: .long, time: .shortened))
     //            Text(Date.now.formatted(date: .long, time: .omitted))
